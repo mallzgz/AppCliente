af8fd62322275d7d8525a9e70ed7587a2b807f88
mallzgz <612200@unizar.es>
2016-04-10 22:38:03 +0200

    -Last upload, new changes in functions.js and index.html

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -1,22 +1,30 @@
-$(document).ready(function() {
+/*$(document).ready(function() {
 	console.log("hola");
-	registerSearch();
+	search();
 	console.log("hola2");
 
-});
-
+});*/
+function search() {
+	$.getJSON("https://books-analyzer.herokuapp.com/" + "search", {book: $("#book").val(), author: $("#author").val(), 
+		character: $("#character").val(), url: $("#url").val()}, function(json) {
+			var plantilla = $('#plantillaTwitter').html();
+			$("#resultsBlock").empty().append(Mustache.to_html(plantilla, json));
+			//$("#resultsBlock").empty().append(JSON.stringify(json));
+		});
+	});	
+}
+/*
 function registerSearch() {
 	$("#search").submit(function(ev){
 		event.preventDefault();
 		$.get($(this).attr('action'), {book: $("#book").val(), author: $("#author").val(), 
 			character: $("#character").val(), link: $("#link").val()}, function(data) {
 			//$("#resultsBlock").empty().append(data);
-			console.log("hola3");
 			var plantilla = $('#plantillaTwitter').html();
     		var html = Mustache.to_html(plantilla, data);
     		$("#resultsBlock").empty().append(html);
 		});	
 	});
-}
+}*/
 
 
