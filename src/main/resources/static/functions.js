6f82ee2f6fd65fdea33137878163e573585831a1
mallzgz <612200@unizar.es>
2016-04-11 00:06:01 +0200

    -New changes

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -1,11 +1,18 @@
 function search() {
-	$.getJSON("https://books-analyzer.herokuapp.com/" + "search", {book: $("#book").val(), author: $("#author").val(), 
-		character: $("#character").val(), url: $("#url").val()}, function(json) {
-			var plantilla = $('#plantillaTwitter').html();
-			$("#resultsBlock").empty().append(Mustache.to_html(plantilla, json));
-			//$("#resultsBlock").empty().append(JSON.stringify(json));
-		});
-	});	
+	console.log("hola");
+	var title= $("#title").val(); 
+	var author= $("#author").val();
+	var character= $("#character").val();
+	var url= $("#url").val();
+	var web="https://books-analyzer.herokuapp.com/" + "search"+"?title="+title+"&author="+author+"&character="+character+"&url="+url;
+	console.log(web);
+	$.ajax({
+	    url: web,
+	    dataType: 'jsonp',
+	    success: function(json){
+	    var plantilla = $('#plantillaTwitter').html();
+		$("#resultsBlock").empty().append(Mustache.to_html(plantilla, json));
+		//$("#resultsBlock").empty().append(JSON.stringify(json));
+	    }
+	});
 }
-
-
