fc479d2016bf35b467e78a7852df28840af981cc
mallzgz <612200@unizar.es>
2016-04-11 00:21:09 +0200

    - Trying

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -4,13 +4,16 @@ function search() {
 	var author= $("#author").val();
 	var character= $("#character").val();
 	var url= $("#url").val();
-	var web="https://books-analyzer.herokuapp.com/" + "search"+"?title="+title+"&author="+author+"&character="+character+"&url="+url;
-	console.log(web);
+	var urlCompleta="https://books-analyzer.herokuapp.com/" + "search"+"?title="+title+"&author="+author+"&character="+character+"&url="+url;
+	console.log(urlCompleta);
 	$.ajax({
-	    url: web,
+	    url: urlCompleta,
 	    dataType: 'jsonp',
+	    cache: 'false',
+	    type: 'GET',
 	    success: function(json){
 	    var plantilla = $('#plantillaTwitter').html();
+		console.log(urlCompleta);
 		$("#resultsBlock").empty().append(Mustache.to_html(plantilla, json));
 		//$("#resultsBlock").empty().append(JSON.stringify(json));
 	    }
